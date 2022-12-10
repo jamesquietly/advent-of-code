74cdb43f46e0dd77e30a34fcaaa0b3bc2d4eba73
jamesquietly <jamesquietly@gmail.com>
2022-12-10 00:22:08 -0800

    finish day 1

--- 2022/day01/day01.ts
+++ 2022/day01/day01.ts
@@ -1,8 +1,44 @@
+const addCalories = (data: number[]) => {
+	const res: number[] = [];
+	let sum = 0;
+	data.forEach(num => {
+		if (num === 0) {
+			res.push(sum);
+			sum = 0;
+		}
+		sum += num;
+	});
+	return res;
+};
+
+const findMostCalories = (data: number[]) => {
+	let bigNum = data[0] || 0;
+	for (let i = 0; i < data.length; i++) {
+		if (data[i] > bigNum) {
+			bigNum = data[i];
+		}
+	}
+	return bigNum;
+};
+
+const part1 = (data: number[]) => {
+	const calories = addCalories(data);
+	const mostCalories = findMostCalories(calories);
+	console.log('most calories:', mostCalories);
+};
+
+const part2 = (data: number[]) => {
+	const calories = addCalories(data);
+	const sortedCalories = calories.sort((a, b) => b - a);
+	const sumTopThree = sortedCalories[0] + sortedCalories[1] + sortedCalories[2];
+	console.log('sum of top 3:', sumTopThree);
+};
+
 const main = async () => {
-	const [inputFile] = Deno.args;
-	const inputData = await Deno.readTextFile(inputFile);
+	const inputData = await Deno.readTextFile('day01.txt');
 	const data = inputData.trim().split('\n').map(Number);
-	console.log('hello world', data);
+	part1(data);
+	part2(data);
 };
 
 await main();
